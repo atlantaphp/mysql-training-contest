7a2689825aa4a98fb3b9b478b5ee28cd75a3d5b8
Ben Ramsey <ramsey@php.net>
2009-11-18 03:21:08 +0000

    Adding the index script and style sheet.

---
+++ deploy/public_html/index.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Atlanta PHP - MySQL Training Raffle</title>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="all" charset="utf-8" />
+</head>
+<body>
+
+    <div id="atlphp">
+
+        <h1>Atlanta PHP</h1>
+
+    </div>
+
+    <div id="details">
+
+        <h2>
+            Register for a chance to win a free pass to Performance Optimization
+            for MySQL with InnoDB and XtraDB.
+        </h2>
+
+        <div id="register">
+
+            <p>
+                Enter your name and email address for a chance to win a pass to
+                the workshop <a href="http://percona-ga-atl.eventbrite.com/" target="top">Performance
+                Optimization for MySQL with InnoDB and XtraDB</a> presented by <strong>Morgan
+                Tocker</strong> and hosted by <a href="http://www.percona.com/" target="top">Percona, Inc.</a>
+            </p>
+
+            <form id="registration">
+
+                <label for="name">Your name:</label>
+                <input type="text" name="name" id="name" /><br />
+
+                <label for="email">Your email:</label>
+                <input type="text" name="email" id="email" /><br />
+
+                <label for="submit">&nbsp;</label>
+                <input type="submit" value="Register!">
+
+            </form>
+
+        </div>
+
+        <div id="info">
+
+            <h3>Contest details</h3>
+
+            <p>
+                The contest opens at 12:01 AM EST on Wednesday, November 18, 2009 and
+                runs until 11:59 PM EST on Tuesday, November 24, 2009. A single winner
+                will be selected in a random drawing and announced on Wednesday,
+                November 25, 2009.
+            </p>
+
+            <p>
+                The winner will receive one (1) pass to Performance Optimization for MySQL
+                with InnoDB and XtraDB to be held at MicroTek on December 10, 2009
+                from 9:30 AM EST to 5:00 PM EST. Morgan Tocker will lead this intensive
+                workshop, covering tuning MySQL when using the InnoDB and XtraDB
+                storage engines.
+            </p>
+
+            <p>
+                For more details, please ask on the <a href="http://groups.google.com/group/atlantaphp">Atlanta
+                PHP Google Group</a> (for the benefit of all our members) or send
+                email to contests at atlantaphp.org.
+            </p>
+
+            <h3>Workshop details</h3>
+
+            <p>
+                <strong>What:</strong>
+                <em>Performance Optimization for MySQL with InnoDB and XtraDB</em><br />
+
+                <strong>When:</strong>
+                <em>Thursday, December 10, 2009 from 9:30 AM - 5:00 PM (ET)</em><br />
+
+                <strong>Where:</strong>
+                <em>MicroTek - Atlanta</em><br />
+
+                <strong>Hosted by:</strong>
+                <em><a href="http://www.percona.com/" target="top">Percona, Inc.</a></em><br />
+
+                <strong>Register:</strong>
+                <em><a href="http://percona-ga-atl.eventbrite.com/" target="top">Sign up at Eventbrite</a></em>
+            </p>
+
+            <h3>Morgan Tocker presents at the Atlanta PHP December meeting!</h3>
+
+            <p>
+                Following the December 10th workshop, Morgan Tocker will present
+                "Quick Wins: Performance Tuning + 3rd Party Patches for MySQL" at
+                the Atlanta PHP December meeting on December 10th at 7:00 PM EST.
+            </p>
+
+            <p>
+                <strong>
+                    For more details and to RSVP for the Atlanta PHP December meeting,
+                    <a href="http://meetup.atlantaphp.org/calendar/11898004/">please visit
+                    Meetup.com</a>.
+                </strong>
+            </p>
+
+        </div>
+    </div>
+
+</body>
+</html>
